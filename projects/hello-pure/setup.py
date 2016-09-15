276a499b659156cd36d3e6662de3c892bcffe0c4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-15 13:48:34 -0400

    Add hello-pure project

---
+++ projects/hello-pure/setup.py
@@ -0,0 +1,10 @@
+from skbuild import setup
+
+setup(
+    name="hello-pure",
+    version="1.2.3",
+    description="a minimal example package",
+    author='The scikit-build team',
+    license="MIT",
+    packages=['hello'],
+)
