c77d65dc7f34f51bc061b92d16a6c63b78491310
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-06-27 18:48:33 -0500

    pybind11 example: merge library implementation into pybind sources
    This is just a little too "extra" for what is supposed to be an example
    of how to use scikit-build with a pybind11 project.

--- projects/hello-pybind11/src/hello/hello_py.cpp
+++ projects/hello-pybind11/src/hello/hello_py.cpp
@@ -1,11 +1,18 @@
-#include "hello.h"
+#include <iostream>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
-using namespace hello;
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int return_two() {
+    return 2;
+}
 
 PYBIND11_MODULE(_hello, m) {
-    m.doc() = "_hello"; // optional module docstring {
-    m.def("hello", &hello::hello);
-    m.def("return_two", &return_two);
+    m.doc() = "_hello";
+    m.def("hello", &hello, "Prints \"Hello, World!\"");
+    m.def("return_two", &return_two, "Returns 2");
 }
