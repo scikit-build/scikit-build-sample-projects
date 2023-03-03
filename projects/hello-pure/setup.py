45f7984daf82bc467494e858120f462fbdd2d839
Henry Schreiner <henryschreineriii@gmail.com>
2023-03-03 16:50:00 -0500

    style: run pre-commit and fix
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- projects/hello-pure/setup.py
+++ projects/hello-pure/setup.py
@@ -1,12 +1,11 @@
 from skbuild import setup
 
-
 setup(
     name="hello-pure",
     version="1.2.3",
     description="a minimal example package (pure python version)",
-    author='The scikit-build team',
+    author="The scikit-build team",
     license="MIT",
-    packages=['hello'],
+    packages=["hello"],
     python_requires=">=3.7",
 )
