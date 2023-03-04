681f16c9e442063df001b90857fd077063c36044
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-03-04 17:28:52 -0500

    ci: skip Windows for Fortran

--- noxfile.py
+++ noxfile.py
@@ -5,8 +5,8 @@ import nox
 
 hello_list = ["hello-pure", "hello-cpp", "hello-pybind11", "hello-cython"]
 if not sys.platform.startswith("win"):
-    hello_list.append("hello-cmake-package")
-long_hello_list = hello_list + ["pen2-cython", "core-c-hello", "core-pybind11-hello", "pi-fortran"]
+    hello_list.extend(["hello-cmake-package", "pi-fortran"])
+long_hello_list = hello_list + ["pen2-cython", "core-c-hello", "core-pybind11-hello"]
 
 
 @nox.session
