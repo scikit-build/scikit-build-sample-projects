0bf54d3a01c3e93a346611f2140b8423b83ff3dd
Ian Carroll <ian.t.carroll@nasa.gov>
2023-03-20 03:32:26 +0000

    incorrect hello-f2py

---
+++ projects/hello-f2py/hello/hello.f90
@@ -0,0 +1,7 @@
+! generate a signature file from this file with:
+! $ python -m numpy.f2py hello.f90 -m _hello -h _hello.pyf
+! and edit if helpful (see F2PY docs)
+subroutine hello(a)
+    character(len=*) :: a
+    print *,"Hello, " // a // "!"
+end subroutine hello
---
+++ projects/hello-f2py/setup.py
@@ -0,0 +1,3 @@
+from skbuild import setup
+
+setup(packages=["hello"])
