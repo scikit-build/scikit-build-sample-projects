8db83f40289068ac79ee3f1300d0c0c3df3f3c8d
Henry Schreiner <henryschreineriii@gmail.com>
2024-06-30 02:37:06 -0400

    feat: free-threaded pybind11
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- projects/hello-free-threading/tests/test_thread.py
+++ projects/hello-free-threading/tests/test_thread.py
@@ -1,5 +1,6 @@
 import freecomputepi.comp
 import freecomputepi.pure
+import freecomputepi.pybind
 import pytest
 
 
@@ -12,4 +13,10 @@ def test_pure(threads):
 @pytest.mark.parametrize("threads", [0, 1, 2, 4])
 def test_comp(threads):
     π = freecomputepi.comp.pi_in_threads(threads, 100_000_000)
+    assert π == pytest.approx(3.1415926535, rel=0.01)
+
+
+@pytest.mark.parametrize("threads", [0, 1, 2, 4])
+def test_pybind(threads):
+    π = freecomputepi.pybind.pi_in_threads(threads, 100_000_000)
     assert π == pytest.approx(3.1415926535, rel=0.01)
